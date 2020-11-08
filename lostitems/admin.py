ca23addf74b9783615ca3627f85028ff016661a5
manoj71 <manojk7371@gmail.com>
2020-11-08 14:17:46 +0530

    update lost section

--- lostitems/admin.py
+++ lostitems/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import item
 # Register your models here.
-<<<<<<< HEAD
-=======
-
->>>>>>> 68a286a713f5cad9365a6da968a5d107e185c2c7
 admin.site.register(item)
